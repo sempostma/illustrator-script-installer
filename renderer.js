6935e1913f22143726139250846c5eedf912fb72
sempostma <sem.postma@gmail.com>
2020-03-19 00:04:39 +0100

    various

--- renderer.js
+++ renderer.js
@@ -7,9 +7,11 @@
 
 
 const Store = require('electron-store');
-const { app } = require('electron').remote
+const { app, dialog, getCurrentWindow } = require('electron').remote
+const mainWindow = getCurrentWindow()
 const fs = require('fs')
 const path = require('path')
+
 
 const store = new Store();
 
@@ -42,39 +44,189 @@ qs('#illustrator-dir-not-found-close').addEventListener('click', e => {
 if (store.get('illustrator-presets-dir')) {
     qs('#illustrator-dir-not-found-message').style.display = 'none'
     qs('#search-illustrator-dir').style.display = 'none'
+} else {
+    qs('#remove-illustrator-dir').style.display = 'none'
 }
+
+qs('#illustrator-location').innerText = store.get('illustrator-presets-dir', __('notFound'))
 
 const isDirectory = source => fs.lstatSync(source).isDirectory()
 
-qs('#search-illustrator-dir').addEventListener('click', e => {
-    var isWin = process.platform === "win32";
-    if (isWin) {
-        const installDir = 'C:/Program Files/Adobe'
-        fs.readdir(installDir, (err, directories) => {
+function tryDirectoryAsIllustratorInstallationDirectory(rootDir) {
+    const presetDir = path.join(rootDir, 'Presets')
+    return new Promise((resolve, reject) => {
+        fs.readdir(presetDir, (err, localeDirs) => {
             if (err) {
                 alert(__('cannotFindIllustratorInstallationPleaseSpecifyManually') + '\n' + err.toString())
+                reject()
             } else {
-                const correctDir = directories.find(dir => dir.startsWith('Adobe Illustrator'))
-
-                const presetDir = path.join(installDir, correctDir, 'Presets')
-
-                fs.readdir(presetDir, (err, localeDirs) => {
-                    localeDirs = localeDirs.map(dir => path.join(presetDir, dir)).filter(isDirectory)
-                    if (err) {
-                        alert(__('cannotFindIllustratorInstallationPleaseSpecifyManually') + '\n' + err.toString())
-                    } else {
-                        if (localeDirs.length === 1) {
-                            const localePresetsDir = path.join(presetDir, localeDirs[0])
-                            store.set('illustrator-presets-dir', localePresetsDir)
-                            alert(__('foundIllustratorDirectory'))
-                        } else {
-                             alert(__('cannotFindIllustratorInstallationPleaseSpecifyManually') + '\nLocale directories ' + localeDirs)
-                        }
-                    }
-                })
+                localeDirs = localeDirs.map(dir => path.join(presetDir, dir)).filter(isDirectory)
+                if (localeDirs.length === 1) {
+                    const localePresetsDir = localeDirs[0]
+                    store.set('illustrator-presets-dir', localePresetsDir)
+                    alert(__('foundIllustratorDirectory'))
+                    resolve()
+                } else {
+                    alert(__('cannotFindIllustratorInstallationPleaseSpecifyManually') + '\nLocale directories ' + localeDirs)
+                    reject()
+                }
             }
         })
-    } else {
-        alert(__(cannotFindIllustratorInstallationPleaseSpecifyManually))
+    })
+}
+
+function findIllustratorDirectory() {
+    return new Promise((resolve, reject) => {
+        var isWin = process.platform === "win32";
+        if (isWin) {
+            const installDir = 'C:/Program Files/Adobe'
+            fs.readdir(installDir, (err, directories) => {
+                if (err) {
+                    alert(__('cannotFindIllustratorInstallationPleaseSpecifyManually') + '\n' + err.toString())
+                    reject()
+                } else {
+                    const correctDir = directories.find(dir => dir.startsWith('Adobe Illustrator'))
+                    if (correctDir) {
+                        const rootDir = path.join(installDir, correctDir)
+                        tryDirectoryAsIllustratorInstallationDirectory(rootDir).then(resolve).catch(reject)
+                    } else {
+                        alert(__('cannotFindIllustratorInstallationPleaseSpecifyManually') + '\n' + __('noAdobeIllustratorFolder'))
+                        reject()
+                    }
+                }
+            })
+        } else {
+            alert(__('cannotFindIllustratorInstallationPleaseSpecifyManually'))
+            reject()
+        }
+    })
+}
+
+qs('#search-illustrator-dir').addEventListener('click', async e => {
+    await findIllustratorDirectory()
+    qs('#illustrator-dir-not-found-message').style.display = 'none'
+    qs('#search-illustrator-dir').style.display = 'none'
+    qs('#illustrator-location').innerText = store.get('illustrator-presets-dir', __('notFound'))
+
+})
+
+qs('#select-illustrator-dir').addEventListener('click', async e => {
+    const result = await dialog.showOpenDialog(mainWindow, {
+        properties: ['openDirectory']
+    })
+    const [filePath] = result.filePaths
+
+    await tryDirectoryAsIllustratorInstallationDirectory(filePath)
+
+    qs('#illustrator-dir-not-found-message').style.display = 'none'
+    qs('#search-illustrator-dir').style.display = 'none'
+    qs('#illustrator-location').innerText = store.get('illustrator-presets-dir', __('notFound'))
+
+})
+
+qs('#remove-illustrator-dir').addEventListener('click', e => {
+    if (confirm(__('areYouSure'))) {
+        store.delete('illustrator-presets-dir')
+        qs('#illustrator-dir-not-found-message').style.display = null
+        qs('#search-illustrator-dir').style.display = null
+        qs('#illustrator-location').innerText = ''
+        qs('#remove-illustrator-dir').style.display = 'none'
     }
+})
+
+function createItem(filePath) {
+    const fileName = path.basename(filePath).replace(path.extname(filePath), '')
+
+
+    var e_2 = document.createElement("tr");
+    var e_3 = document.createElement("td");
+    e_3.setAttribute("class", "collapsing");
+    var e_4 = document.createElement("div");
+    e_4.setAttribute("class", "ui checkbox");
+    var e_5 = document.createElement("input");
+    e_5.setAttribute("type", "checkbox");
+    e_5.setAttribute("checked", "true");
+    e_4.appendChild(e_5);
+    var e_6 = document.createElement("label");
+    e_4.appendChild(e_6);
+    e_3.appendChild(e_4);
+    e_2.appendChild(e_3);
+    var e_7 = document.createElement("td");
+    e_7.appendChild(document.createTextNode(fileName));
+    e_2.appendChild(e_7);
+    var e_8 = document.createElement("td");
+    e_8.appendChild(document.createTextNode("September 14, 2013"));
+    e_2.appendChild(e_8);
+    var e_9 = document.createElement("td");
+    e_9.appendChild(document.createTextNode("jhlilk22@yahoo.com"));
+    e_2.appendChild(e_9);
+    var e_10 = document.createElement("td");
+    e_10.appendChild(document.createTextNode("No"));
+    e_2.appendChild(e_10);
+    return e_2;
+}
+
+function renderScriptRows() {
+    var list = qs('#scripts-rows-tbody')
+
+    const location = store.get('illustrator-presets-dir')
+    if (!location) return
+
+    while (list.children.length) list.removeChild(list.firstElementChild)
+
+    const scriptsDir = path.join(location, 'Scripts')
+
+    fs.readdir(scriptsDir, (err, files) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        const scripts = files.filter(x => x.endsWith('.jsx'))
+
+        for (let i = 0; i < scripts.length; i++) {
+            const script = scripts[i];
+            list.appendChild(
+                createItem(
+                    path.join(
+                        scriptsDir,
+                        script
+                    )
+                )
+            )
+        }
+    })
+}
+
+qs('#add-script').addEventListener('click', e => {
+    $('#add-script-modal').modal('show')
+})
+
+qs('#refresh-illustrator-scripts').addEventListener('click', e => {
+    renderScriptRows()
+})
+
+renderScriptRows()
+
+Array.from(qa('[data-tab-controls]')).forEach(tabButton => {
+    tabButton.addEventListener('click', e => {
+        for (let i = 0; i < tabButton.parentElement.children.length; i++) {
+            const element = tabButton.parentElement.children[i];
+            if (element.classList.contains('active') &&
+                tabButton !== element) {
+                element.classList.remove('active')
+            }
+        }
+        tabButton.classList.add('active')
+        const tab = qs(`#${tabButton.getAttribute('data-tab-controls')}`)
+        for (let i = 0; i < tab.parentElement.children.length; i++) {
+            const element = tab.parentElement.children[i];
+            if (element.classList.contains('tab') &&
+                element.classList.contains('active') &&
+                element.getAttribute('id') !== element.getAttribute('data-tab-controls')) {
+
+                element.classList.remove('active')
+            }
+        }
+        tab.classList.add('active')
+    })
 })
